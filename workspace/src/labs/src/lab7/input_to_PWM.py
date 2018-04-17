180232b54d9c2aa790189c7c00366156b7269808
Ryan Cosner <rkcosner@gmail.com>
2018-04-17 16:57:40 -0700

    Added the callback function for the current state of the velocity. The new variable is called 'v_meas'

--- workspace/src/labs/src/lab7/input_to_PWM.py
+++ workspace/src/labs/src/lab7/input_to_PWM.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 # Author: Tony Zheng
-# MEC231A BARC Project 
+# MEC231A BARC Project
 
 import rospy
 import time
 from geometry_msgs.msg import Twist
-from barc.msg import ECU, Input, Moving
+from barc.msg import ECU, Input, Moving, encoder
 
 
 
@@ -30,16 +30,25 @@ def moving_callback_function(data):
         move = False
         still_moving = False
 
+
+def encoder_callback_function(data):
+    global v_meas
+    # determine the measured velocity from the encoder data
+    bandw = 0.200 # sec, system frequency = 200 Hz
+    vL=(3*data.FL - 4*thetaL1 + thetaL2)/(2*bandw)  # Provides us with the approximated angular velocity L
+    vR=(3*data.FR - 4*thetaR1 + thetaR2)/(2*bandw)  # Provides us with the approximated angular velocity R
+    v_meas=((vL+vR)/2)*(2*pi/8)*r_tire  # Calculate the linear velocity: (EncoderConts/Sec)*(Radians/Encoder)*Radius
+
 # update
 def callback_function(data):
-    global move, still_moving
+    global move, still_moving, v_meas
     ################################################################################################################################################
     # Convert the velocity into motorPWM and steering angle into servoPWM
     a_motor, b_motor = -0.6055, 0.0129
     a_servo, b_servo = -0.0006100255, 0.96345
     # newECU.motor =  (data.vOpt - a_motor*current_vel) / b_motor
-    newECU.motor =  (data.vOpt - a_motor * 0.5) / b_motor
-    newECU.servo = (data.deltaOpt - b_servo) / a_servo 
+    newECU.motor =  (data.vOpt - a_motor * v_meas) / b_motor
+    newECU.servo = (data.deltaOpt - b_servo) / a_servo
     #################################################################################################################################################
 
     maxspeed = 1575
@@ -64,12 +73,12 @@ def callback_function(data):
     pubname.publish(newECU)
 # state estimation node
 def inputToPWM():
-    
+
     # initialize node
     rospy.init_node('inputToPWM', anonymous=True)
-    
+
     global pubname , newECU , subname, move , still_moving
-    newECU = ECU() 
+    newECU = ECU()
     newECU.motor = 1500
     newECU.servo = 1550
     move = False
@@ -81,12 +90,14 @@ def inputToPWM():
     rospy.Subscriber('turtle1/cmd_vel', Twist, start_callback)
     subname = rospy.Subscriber('uOpt', Input, callback_function)
     rospy.Subscriber('moving', Moving, moving_callback_function)
+    rospy.Subscriber('encoder', Encoder, encoder_callback_function)
+
     # set node rate
     loop_rate   = 40
     ts          = 1.0 / loop_rate
     rate        = rospy.Rate(loop_rate)
     t0          = time.time()
-     
+
     rospy.spin()
 
 
